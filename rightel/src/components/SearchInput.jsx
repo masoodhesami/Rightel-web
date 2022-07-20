bcd680b37d836e69714a011fce42a65cef3b1aee
Masoud Hesami <masoodhesami79@gmail.com>
2022-07-21 02:09:50 +0430

    searchBox display on mobile fixed

--- rightel/src/components/SearchInput.jsx
+++ rightel/src/components/SearchInput.jsx
@@ -3,7 +3,7 @@ import { Box, InputGroup, InputLeftElement, Input } from '@chakra-ui/react'
 
 const SearchInput = () => {
     return (
-        <Box w={{ lg: '185px', md: '185px', sm: '150px' }} h='45'>
+        <Box display={{lg:'inline-block',md: 'inline-block',sm:'none'}} w={{ lg: '185px', md: '185px', sm: '150px'}} h='45'>
             <InputGroup>
                 <InputLeftElement
                     pointerEvents='none'
