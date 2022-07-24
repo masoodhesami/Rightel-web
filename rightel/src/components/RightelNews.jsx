16306a7254f7dd9f65097c347fe68212cb887dbd
Masoud Hesami <masoodhesami79@gmail.com>
2022-07-24 21:09:41 +0430

    skeleton added to homePage

--- rightel/src/components/RightelNews.jsx
+++ rightel/src/components/RightelNews.jsx
@@ -1,25 +1,16 @@
-import { SimpleGrid, Box, Skeleton, Image } from '@chakra-ui/react'
-import { useState } from 'react';
+import { SimpleGrid, Box } from '@chakra-ui/react'
 import NewsBanner from './NewsBanner';
 import NewsSlider from "./NewsSlider";
 
 const RightelNews = () => {
-    const [isLoaded, setIsLoaded] = useState(false);
-    setTimeout(() => {
-        setIsLoaded(true)
-    }, 1500);
     return (
         <SimpleGrid mb={5} columns={{ lg: '2', md: '2', sm: '1' }} spacingX='40px' spacingY='35px'>
-            <Skeleton isLoaded={isLoaded}>
-                <Box>
-                    <NewsSlider />
-                </Box>
-            </Skeleton>
-            <Skeleton isLoaded={isLoaded}>
-                <Box>
-                    <NewsBanner />
-                </Box>
-            </Skeleton>
+            <Box>
+                <NewsSlider />
+            </Box>
+            <Box>
+                <NewsBanner />
+            </Box>
         </SimpleGrid>
 
     );
--- rightel/src/screens/HomePage.jsx
+++ rightel/src/screens/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Skeleton } from '@chakra-ui/react'
 import Header from "../components/Header";
 import MyRightel from '../components/MyRightel';
 import Navbar from '../components/Navbar.tsx';
@@ -7,21 +7,32 @@ import Services from '../components/Services';
 import SlideShow from '../components/SlideShow';
 import Suggestion from '../components/Suggestion';
 import TextHeader from '../components/TextHeader';
+import { useState } from 'react';
 
 
 const HomePage = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    setTimeout(() => {
+        setIsLoaded(true)
+    }, 2000);
     return (
         <Container maxW='container.xl' color='white'>
             <Header />
             <Navbar />
-            <SlideShow />
-            <Services />
+            <Skeleton isLoaded={isLoaded}>
+                <SlideShow />
+            </Skeleton>
+            <Skeleton isLoaded={isLoaded}>
+                <Services />
+            </Skeleton>
             <TextHeader mainTitle='پیشنهادات ویژه' subTitle='ویژه' detail='خدمات ارتباطی نوین' setDivider={true} />
             <Suggestion />
             <TextHeader mainTitle='اپلیکیشن رایتل من' subTitle='' detail='' setDivider={false} />
             <MyRightel />
             <TextHeader mainTitle='اخبار رایتل' subTitle='' detail='' setDivider={false} />
-            <RightelNews />
+            <Skeleton isLoaded={isLoaded}>
+                <RightelNews />
+            </Skeleton>
         </Container>
     );
 }
